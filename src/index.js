a0b8578796b6106d87002606d73a8db265837379
Wilson Santos <contato@wilsonsantos.net>
2022-03-24 11:00:09 -0300

    Add initial routes and middleware
    * Add POST /users
    * Add GET /todos
    * Add POST /todos

--- src/index.js
+++ src/index.js
@@ -1,29 +1,69 @@
 const express = require('express');
 const cors = require('cors');
+const {v4: uuidv4} = require('uuid');
 
-// const { v4: uuidv4 } = require('uuid');
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(400).json({error: 'User not found'});
+  }
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  
+  const id = uuidv4();
+
+  const user = {
+    id,
+    name,
+    username,
+    todos: [],
+  };
+
+  users.push(user);
+
+
+  return response.status(201).json(user);
+
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request;
+  return response.status(200).json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request;
+  const {title, deadline} = request.body;
+  const id = uuidv4();
+
+  const todo = {
+    id,
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    cratedAt: new Date(),
+  };
+
+  user.todos.push(todo);
+  return response.status(201).json(todo);
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
