abcafaf0dbdeb8f750d57c33e017b917d1bd9fa9
Wilson Santos <contato@wilsonsantos.net>
2022-03-25 09:42:27 -0300

    Add route to update todo
    I lost too much time because I forget var is a reference and I can change directly from the "find" method. Was more hard think how work with memory and not database.

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ function checksExistsUserAccount(request, response, next) {
   const {username} = request.headers;
   
   const user = users.find(user => user.username === username);
+  
 
   if (!user) {
     return response.status(400).json({error: 'User not found'});
@@ -67,7 +68,16 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request;
+  const {id} = request.params;
+  const {title,deadline} = request.body;
+
+  const todo = user.todos.find(todo => todo.id === id);
+  
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.status(200).json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
