235c85ebf01b5220b4975ca570648d8ceef5b214
Wilson Santos <contato@wilsonsantos.net>
2022-03-25 14:24:04 -0300

    Add verification on TODO update
    Check if todo exists before update

--- src/index.js
+++ src/index.js
@@ -87,6 +87,10 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 
   const todo = user.todos.find(todo => todo.id === id);
 
+  if(!todo){
+    return response.status(404).json({error:'Todo not found'});
+  }
+
   todo.title = title;
   todo.deadline = new Date(deadline);
 
